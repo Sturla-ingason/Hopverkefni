a589bfb87834d428bef96c61cbc033c61b6f60ff
Sturla-ingason <Sturladi@gmail.com>
2024-03-19 13:31:13 +0000

    Breyt nöfnum a filum í StrengirController og StrengirApplication til að vera í samræmi við verkefni

--- src/main/java/com/example/hopverkefni/StrengirController.java
+++ src/main/java/com/example/hopverkefni/StrengirController.java
@@ -3,7 +3,7 @@ package com.example.hopverkefni;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StrengirController {
     @FXML
     private Label welcomeText;
 
